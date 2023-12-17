3106269864c18a2e8d6f9b37f64756a2ccb0e988
caiocsm-sudo <stampedesw29@gmail.com>
2023-12-17 15:49:11 -0300

    add task modal in creation

--- src/components/add-todo/AddTodo.tsx
+++ src/components/add-todo/AddTodo.tsx
@@ -1,7 +1,26 @@
-export default function AddTodo() {
-  return (
-    <div>
+import { BaseSyntheticEvent, useState } from "react";
+import Button from "../defaults/Button";
 
+export default function AddTodo() {
+  const [title, setTitle] = useState<string>("");
+
+  const handleSubmit = () => {
+    const data = localStorage.setItem("todo", title);
+    console.log("submitted " + data);
+  };
+
+  return (
+    <div className="box-shadow">
+      <div>
+        <form>
+          <input
+            type="text"
+            onChange={(e: BaseSyntheticEvent) => setTitle(e.target.value)}
+            value={title}
+          />
+          <Button color="primary">{"Create Task"}</Button>
+        </form>
+      </div>
     </div>
   );
 }
